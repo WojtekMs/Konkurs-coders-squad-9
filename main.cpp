2dc0cb12b5fc8e9a53e8313f3ea030302fdeb60f
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-10 23:18:22 +0200

    print1 + print2 functions

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+#include "task2.hpp"
+
+
+int main() {
+    std::vector<int> vec {2, 7, 8, 9, 10, 11, 18, 22};
+    print2(vec);
+}
---
+++ task2.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+
+void print1(const std::vector<int>& vec) {
+    std::equal(vec.begin(), vec.end(), vec.begin(), [](const int first, const int second)
+    {
+        std::cout << first << ", ";
+        return first == second;
+    });
+
+}
+
+void print2(const std::vector<int>& vec) {
+    std::any_of(vec.begin(), vec.end(), [](const int element)
+    {
+        std::cout << element << ", ";
+        return false;
+    });
+
+}
