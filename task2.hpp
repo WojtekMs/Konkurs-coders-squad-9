c251c27db2ef2dd9157c58a2bc253d93afbd86b9
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-10 23:56:09 +0200

    print12 added

--- task2.hpp
+++ task2.hpp
@@ -100,3 +100,15 @@ void print11(const std::vector<int>& vec) {
         return false;
     });
 }
+
+void print12(const std::vector<int>& vec) {
+    std::adjacent_find(vec.begin(), vec.end(), [i{0}](const int first, const int second) mutable
+    {
+        if (i == 0) {
+            std::cout << first << ", ";
+        }
+        i++;
+        std::cout << second << ", ";
+        return false;
+    });
+}
