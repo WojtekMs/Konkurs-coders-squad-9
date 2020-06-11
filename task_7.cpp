591a8d763716a22884d2650eae045d4ff2a650fa
Wojciech Matusiak <253035@student.pwr.edu.pl>
2020-06-11 16:54:23 +0200

    deleteDuplicats function update

--- task_7.cpp
+++ task_7.cpp
@@ -10,7 +10,24 @@ std::string deleteDuplicats(std::string& str){
     std::istringstream ss(str);
     std::vector<std::string> vec((std::istream_iterator<std::string>(ss)),
     std::istream_iterator<std::string>());
-    auto last = std::unique(vec.begin(), vec.end());
+    auto last = std::unique(vec.begin(), vec.end(), [](const std::string& first, const std::string& second)
+    {
+        std::string temp_first(first);
+        std::string temp_second(second);
+        auto last1 = std::remove_copy_if(first.begin(), first.end(), temp_first.begin(), 
+        [](const unsigned char element)
+        {
+            return std::ispunct(static_cast<unsigned char>(element));
+        });
+        auto last2 = std::remove_copy_if(second.begin(), second.end(), temp_second.begin(), 
+        [](const unsigned char element)
+        {
+            return std::ispunct(static_cast<unsigned char>(element));
+        });
+        temp_first.erase(last1, temp_first.end());
+        temp_second.erase(last2, temp_second.end());
+        return temp_first == temp_second;
+    });
     vec.erase(last, vec.end());
     
     return std::accumulate(std::next(vec.begin()), vec.end(), vec.front(), [](std::string a, std::string b){
@@ -21,8 +38,8 @@ std::string deleteDuplicats(std::string& str){
 
 
 int main(){
-    std::string str{"Ala ma kota kot ma Ale Ale"};
+    std::string str{"Ala ma ma kota, a kot ma ma Ale Ale."};
     std::cout << deleteDuplicats(str);
-    
     return 0;
+
 }
